b23968adec930bcd9eea09dbf91850fa59ce8122
eyupodeal <eyup@ode.al>
2018-03-29 15:03:25 +0300

    Swift-4 Convert

--- DeviceRotationOnlyOneViewController/PresentedViewController.swift
+++ DeviceRotationOnlyOneViewController/PresentedViewController.swift
@@ -9,17 +9,18 @@
 import UIKit
 
 class PresentedViewController: UIViewController {
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-    
+
     @IBAction func didDismissButtonPress(sender: UIButton) {
-        self.dismissViewControllerAnimated(true) { () -> Void in
-            UIDevice.currentDevice().setValue(Int(UIInterfaceOrientation.Portrait.rawValue), forKey: "orientation")
+        self.dismiss(animated: true) { () -> Void in
+            UIDevice.current.setValue(Int(UIInterfaceOrientation.portrait.rawValue), forKey: "orientation")
         }
     }
-    
+
     func canRotate() -> Void {}
 }
+
